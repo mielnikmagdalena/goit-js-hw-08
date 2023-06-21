acf55b54d005253fd7f71d313e6b77edfd769675
mielnikmagdalena <mielnikmagdalena@gmail.com>
2023-06-21 17:12:37 +0200

    the latest changes of HW8 21.06

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,4 @@
-import SimpleLightbox from 'simple-lightbox';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
